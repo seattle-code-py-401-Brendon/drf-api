4df7c25349aa00b6c47b699e485b4050ae3c49a5
BrendonLH <Brendonhampton1988@gmail.com>
2022-08-28 10:39:15 -0700

    added tests for the models for blogging

--- blogger_app/tests.py
+++ blogger_app/tests.py
@@ -1,12 +1,13 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from .models import Blog
+from datetime import date
 
 
 class BlogTests(TestCase):
 
     def blog_setup(self):
-        self.blog = Blog.objects.create(title='First Blog', subject=None, post_date='8/28/2022', author='Brendon')
+        self.blog = Blog.objects.create(title='First Blog', subject=None, post_date=date.today, author='Brendon')
 
     def test_blog_name(self):
         self.assertEqual(f'{self.blog.title}', 'First Blog')
@@ -18,5 +19,7 @@ class BlogTests(TestCase):
         self.assertIs(self.blog.subject, None)
 
     def test_post_date(self):
-        current_date = 
-        self.assertIs(self.blog.post_date, )
+        self.assertIs(self.blog.post_date, date.today)
+
+    def test_author(self):
+        self.assertEqual(self.blog.author, 'Brendon')
