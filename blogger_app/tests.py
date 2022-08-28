5facac6da6d683b9a18c54bfebeec29a80515fac
BrendonLH <Brendonhampton1988@gmail.com>
2022-08-28 10:35:19 -0700

    pycharm causing lag issues,switching to vscode

--- blogger_app/tests.py
+++ blogger_app/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Blog
 
-# Create your tests here.
+
+class BlogTests(TestCase):
+
+    def blog_setup(self):
+        self.blog = Blog.objects.create(title='First Blog', subject=None, post_date='8/28/2022', author='Brendon')
+
+    def test_blog_name(self):
+        self.assertEqual(f'{self.blog.title}', 'First Blog')
+
+    def test_not_blog_name(self):
+        self.assertIsNot(f'{self.blog.title}', 'Second Blog')
+
+    def test_subject(self):
+        self.assertIs(self.blog.subject, None)
+
+    def test_post_date(self):
+        current_date = 
+        self.assertIs(self.blog.post_date, )
